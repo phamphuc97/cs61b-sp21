e5584cb69b39b1dd95d073824c935570ff4b882d
Phuc Pham <phucpham@Phucs-MacBook-Air.local>
2022-05-18 02:07:17 -0700

    done with lab2

---
+++ lab2/DebugExercise/DebugExercise1.java
@@ -0,0 +1,36 @@
+package DebugExercise;
+
+/**
+ * Exercise for learning how the debug, breakpoint, and step-into
+ * feature work.
+ */
+public class DebugExercise1 {
+    public static int divideThenRound(int top, int bottom) {
+        int quotient = top / bottom;
+        int result = Math.round(quotient);
+        return result;
+    }
+
+    public static double divideThenRound(double top, double bottom) {
+        double quotient = top / bottom;
+        double result = Math.round(quotient);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int t = 10;
+        int b = 2;
+        int result = divideThenRound(t, b);
+        System.out.println("round(" + t + "/" + b + ")=" + result);
+
+        int t2 = 9;
+        int b2 = 4;
+        int result2 = divideThenRound(t2, b2);
+        System.out.println("round(" + t2 + "/" + b2 + ")=" + result2);
+
+        double t3 = 3;
+        double b3 = 4;
+        double result3 = divideThenRound(t3, b3);
+        System.out.println("round(" + t3 + "/" + b3 + ")=" + result3);
+    }
+}
